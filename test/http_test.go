0b4d8455f40afa55fba69c40a91ed8d89c18c9a1
Loafter <mopper@yandex.ru>
2015-07-15 19:54:11 +0300

    add fix GetSize function

--- test/http_test.go
+++ test/http_test.go
@@ -5,7 +5,18 @@ import (
 	"godownloader/http"
 )
 func TestGetSize(t *testing.T) {
- httpclient.GetSize("http://img1.goodfon.ru:80/original/1920x1080/3/4a/mlechnyy-put-kosmos-zvezdy-3734.jpg")
+ if _,e:=httpclient.GetSize("http://img1.goodfon.ru:80/original/1920x1080/3/4a/mlechnyy-put-kosmos-zvezdy-3734.jpg");(e!=nil){
+	 t.Error("failed: Get size must be without error")
+	}
+
+	if _,e:=httpclient.GetSize("http://img1.goodffon.ru:80/original/1920x1080/3/4a/mlechnyy-put-kosmos-zvezdy-3734.jpg");(e==nil){
+		t.Error("failed: Get size must with error")
+	}
+
+	if _,e:=httpclient.GetSize("http://img1.goodfon.ru:80/original/1920x1080/3/4a/mlechnyy-put-kdsfosmos-zvezdy-3734.jpg");(e==nil){
+		t.Error("failed: Get size must be with error")
+	}
+
 }
 
 func TestResume(t *testing.T) {
