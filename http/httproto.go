7badef8628eeb0fcb225cdbfb54e537c284fa69c
Loafter <mopper@yandex.ru>
2015-07-15 19:48:37 +0300

    add fix GetSize function

--- http/httproto.go
+++ http/httproto.go
@@ -2,6 +2,7 @@ package httpclient
 import (
 	"net/http"
 	"log"
+	"errors"
 )
 func TestMultithread(url string) (bool, error) {
 	return false, nil
@@ -11,7 +12,12 @@ func GetSize(urls string) (int64, error) {
 	cl:=http.Client{}
 	resp,err:=cl.Head(urls)
 	if err!=nil{
+		log.Printf("error: when try get file size %v \n",err)
 		return 0,err
+	}
+	if resp.StatusCode!=200{
+		log.Printf("error: file not found or moved status:",resp.Status)
+		return 0,errors.New("error: file not found or moved")
 	}
 	log.Printf("info: file size is %d bytes \n",resp.ContentLength)
 	return resp.ContentLength,nil
