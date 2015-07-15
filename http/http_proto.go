2211bd3e4c4241d828de634a6a2bad2e3e795cde
Loafter <mopper@yandex.ru>
2015-07-15 23:35:57 +0300

    add dummy check multipart download support

--- http/http_proto.go
+++ http/http_proto.go
@@ -4,9 +4,26 @@ import (
 	"log"
 	"errors"
 )
-func TestMultithread(url string) (bool, error) {
-	return false, nil
-}
+func CheckMultipart(urls string) bool {
+	r,err:=http.NewRequest("get",urls,nil)
+	if err!=nil{
+		return false
+	}
+	cl:=http.Client{}
+	resp,err:=cl.Do(r)
+	if err!=nil{
+		log.Printf("error: can't check multipart support assume no %v \n",err)
+		return false
+	}
+	if resp.StatusCodes!=200{
+		log.Printf("error: file not found or moved status:",resp.StatusCode)
+		return 0,errors.New("error: file not found or moved")
+	}
+	if (resp.ContentLength==1) {
+		log.Printf("info: file size is %d bytes \n", resp.ContentLength)
+		return resp.ContentLength, nil
+
+	}}
 
 func GetSize(urls string) (int64, error) {
 	cl:=http.Client{}
