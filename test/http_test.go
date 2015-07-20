935181086b118105e088a1e80dc5a53d53113850
Loafter <mopper@yandex.ru>
2015-07-20 20:23:41 +0300

    add multipart support check

--- test/http_test.go
+++ test/http_test.go
@@ -17,17 +17,14 @@ func TestMultiThreadSuppurt(t *testing.T) {
 	if _, e := httpclient.CheckMultipart("http://s0.cyberciti.org/images/misc/static/2012/11/ifdadta-welcome-0.png"); (e==nil) {
 		t.Error("failed: CheckMultipart size must be with error")
 	}
-
 }
 func TestGetSize(t *testing.T) {
 	if _, e := httpclient.GetSize("http://static.oper.ru/data/gallery/l1048752856.jpg"); (e!=nil) {
 		t.Error("failed: Get size must be without error")
 	}
-
 	if _, e := httpclient.GetSize("http://static.oper.ru/data/gallery/l104d8752856.jpg"); (e==nil) {
 		t.Error("failed: Get size must be with error")
 	}
-
 	if _, e := httpclient.GetSize("http://sdtatic.oper.ru/data/gallery/l1048752856.jpg"); (e==nil) {
 		t.Error("failed: Get size must be with error")
 	}
